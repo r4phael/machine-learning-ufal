7d0016c379c49b02ab8659fb1c9fff16e39514b4
r4phael <pto.raphael@gmail.com>
2018-01-25 04:56:13 -0300

    Algorithms

--- research/ml-elastic.R
+++ research/ml-elastic.R
@@ -146,6 +146,32 @@ executeC50 <- function(dataset, folds) {
   
 }
 
+createDf <- function (){
+  results <<-
+    data.frame(
+      Project = character(),
+      Algorithm = character(),
+      Precision = character(),
+      Recall = character(),
+      "F-Measure" = character()
+    )
+}
+
+finalResults <- function(resultsAlgo, project, algo){
+  results <<-
+    rbind(
+      results,
+      data.frame(
+        "Project" = project,
+        "Algorithm" = algo,
+        "Precision" = median(sapply(resultsAlgo, "[[", 1)),
+        "Recall" = median(sapply(resultsAlgo, "[[", 2)),
+        "F-Measure" = median(sapply(resultsAlgo, "[[", 3))
+      )
+    )
+}
+
+
 
 #####################################################################################################
 #                               Results Balanced                                                    #  
@@ -155,8 +181,99 @@ filenames = list.files(path = "/home/r4ph/R/machine-learning-ufal/datasets/vulne
                        full.names = TRUE,
                        recursive = TRUE)
 
+projects <- c("GlibC", "Httpd", "Kernel", "Mozilla", "Xen")
+setwd("/home/r4ph/R/machine-learning-ufal/datasets/vulnerability/results/")
+
 #Create data frame:
-resultsBal <-
+createDf()
+
+#Apply Results Balanced
+for (i in 1:length(filenames)) {
+  
+  cat("Writting ")
+  
+  dataset <- read.csv(filenames[i])
+  #project <- strsplit(filenames, split = '/')[[i]][9]
+  folds <- createFolds(dataset[1:27], k = 10, returnTrain = TRUE)
+  
+  rows <- nrow(dataset)
+  cat(paste0("Input: ", rows, " rows in project... ", projects[i], "\n"))
+  
+  #Results C50
+  finalResults(executeC50(dataset, folds), projects[i], "C50")
+  #Results Bayes
+  finalResults(executeNaiveBayes(dataset, folds), projects[i], "NaiveBayes")
+  #Algorithm SVM
+  finalResults(executeSVM(dataset, folds), projects[i], "SVM")
+  #Jr48
+  finalResults(executeJ48(dataset, folds), projects[i], "Jr48")
+  #OneR
+  finalResults(executeOneR(dataset, folds), projects[i], "OneR")
+  #RandomForest
+  finalResults(executeRandomForest(dataset, folds), projects[i], "RandomForest")
+  
+}
+
+write.csv(x =  results, file = "ml_balanced.csv")
+
+
+
+#####################################################################################################
+#                               Results With Features Selection                                     #  
+#####################################################################################################
+
+#Create data frame:
+createDf()
+
+#Apply Results Balanced
+for (i in 1:length(filenames)) {
+  
+  features <- makeClassifTask(id = deparse(substitute(dataset)), dataset, colnames(dataset)[28] )
+  fv = generateFilterValuesData(features, method = "information.gain")
+  filtered = filterFeatures(features, fval = fv, threshold = 0.2)
+  
+  filtered
+  
+  cat("Writting ")
+  
+  dataset <- read.csv(filenames[i])
+  #project <- strsplit(filenames, split = '/')[[i]][9]
+  folds <- createFolds(dataset[1:27], k = 10, returnTrain = TRUE)
+  
+  rows <- nrow(dataset)
+  cat(paste0("Input: ", rows, " rows in project... ", projects[i], "\n"))
+  
+  #Results C50
+  finalResults(executeC50(dataset, folds), projects[i], "C50")
+  #Results Bayes
+  finalResults(executeNaiveBayes(dataset, folds), projects[i], "NaiveBayes")
+  #Algorithm SVM
+  finalResults(executeSVM(dataset, folds), projects[i], "SVM")
+  #Jr48
+  finalResults(executeJ48(dataset, folds), projects[i], "Jr48")
+  #OneR
+  finalResults(executeOneR(dataset, folds), projects[i], "OneR")
+  #RandomForest
+  finalResults(executeRandomForest(dataset, folds), projects[i], "RandomForest")
+  
+}
+
+#Finals Results Unbal:
+write.csv(x = results, file = "ml_features.csv")
+
+
+
+
+#####################################################################################################
+#                               Results Unbalanced                                                    #  
+#####################################################################################################
+
+filenamesUnbal = list.files(path = "/home/r4ph/R/machine-learning-ufal/datasets/vulnerability/unbalanced",
+                       full.names = TRUE,
+                       recursive = TRUE)
+
+#Create data frame:
+results <<-
   data.frame(
     Project = character(),
     Algorithm = character(),
@@ -168,158 +285,31 @@ resultsBal <-
 
 #Apply Results Balanced
 for (i in 1:length(filenames)) {
-  folds <- createFolds(elements, k = 10, returnTrain = TRUE)
   
-  dataset <- read.csv(filenames[i], stringsAsFactors = FALSE)
-  project <- strsplit(filenames, split = '/')[[i]][9]
+  cat("Writting ")
   
-  resultsC50 <- executeC50(elements, folds)
-  resultsBayes <- executeNaiveBayes(elements, folds)
-  resultsSVM <- executeSVM(elements, folds)
-  #resultsJ48 <- executeJ48(elements, folds)
-  #resultsOneR <- executeOneR(elements, folds)
-  resultsRF <- executeRandomForest(elements, folds)
+  dataset <- read.csv(filenames[i])
+  #project <- strsplit(filenames, split = '/')[[i]][9]
+  folds <- createFolds(dataset[1:27], k = 10, returnTrain = TRUE)
   
-  resultsBal <-
-    rbind(
-      resultsBal,
-      data.frame(
-        "Project" = project,
-        "Algorithm" = "C50",
-        Precision = median(sapply(resultsC50, "[[", 1)),
-        "Recall" = median(sapply(resultsC50, "[[", 2)),
-        "F-Measure" = median(sapply(resultsC50, "[[", 3))
-      )
-    )
+  rows <- nrow(dataset)
+  cat(paste0("Input: ", rows, " rows in project... ", projects[i], "\n"))
   
-  resultsBal <-
-    rbind(
-      resultsBal,
-      data.frame(
-        "Project" = project,
-        "Algorithm" = "Naive Bayes",
-        Precision = median(sapply(resultsBayes, "[[", 1)),
-        "Recall" = median(sapply(resultsBayes, "[[", 2)),
-        "F-Measure" = median(sapply(resultsBayes, "[[", 3))
-      )
-    )
-  
-  resultsBal <-
-    rbind(
-      resultsBal,
-      data.frame(
-        "Project" = project,
-        "Algorithm" = "SVM",
-        Precision = median(sapply(resultsSVM, "[[", 1)),
-        "Recall" = median(sapply(resultsSVM, "[[", 2)),
-        "F-Measure" = median(sapply(resultsSVM, "[[", 3))
-      )
-    )
-  
-  resultsBal <-
-    rbind(
-      resultsBal,
-      data.frame(
-        "Project" = project,
-        "Algorithm" = "Ramdom Forest",
-        Precision = median(sapply(resultsRF, "[[", 1)),
-        "Recall" = median(sapply(resultsRF, "[[", 2)),
-        "F-Measure" = median(sapply(resultsRF, "[[", 3))
-      )
-    )
+  #Results C50
+  finalResults(executeC50(dataset, folds), projects[i], "C50")
+  #Results Bayes
+  finalResults(executeNaiveBayes(dataset, folds), projects[i], "C50")
+  #Algorithm SVM
+  finalResults(executeSVM(dataset, folds), projects[i], "C50")
+  #Jr48
+  finalResults(executeJ48(dataset, folds), projects[i], "C50")
+  #OneR
+  finalResults(executeOneR(dataset, folds), projects[i], "C50")
+  #RandomForest
+  finalResults(executeRandomForest(dataset, folds), projects[i], "C50")
   
 }
-
-
-#####################################################################################################
-#                               Results Unbalanced                                                    #  
-#####################################################################################################
-
-filenames = list.files(path = "/home/r4ph/R/machine-learning-ufal/datasets/vulnerability/unbalanced",
-                       full.names = TRUE,
-                       recursive = TRUE)
-
-#Create data frame:
-resultsUnbal <-
-  data.frame(
-    Project = character(),
-    Algorithm = character(),
-    Precision = character(),
-    Recall = character(),
-    "F-Measure" = character()
-  )
-
-
-
-for (i in 1:length(filenames)) {
-  folds <- createFolds(elements, k = 10, returnTrain = TRUE)
-  
-  dataset <- read.csv(filenames[i], stringsAsFactors = FALSE)
-  dataset$Fold <- NULL
-  project <- strsplit(filenames, split = '/')[[i]][9]
-  
-  resultsC50 <- executeC50(elements, folds)
-  resultsBayes <- executeNaiveBayes(elements, folds)
-  resultsSVM <- executeSVM(elements, folds)
-  resultsRF <- executeRandomForest(elements, folds)
-  
-  resultsUnbal <-
-    rbind(
-      resultsUnbal,
-      data.frame(
-        "Project" = project,
-        "Algorithm" = "C50",
-        Precision = median(sapply(resultsC50, "[[", 1)),
-        "Recall" = median(sapply(resultsC50, "[[", 2)),
-        "F-Measure" = median(sapply(resultsC50, "[[", 3))
-      )
-    )
-  
-  resultsUnbal <-
-    rbind(
-      resultsUnbal,
-      data.frame(
-        "Project" = project,
-        "Algorithm" = "Naive Bayes",
-        Precision = median(sapply(resultsBayes, "[[", 1)),
-        "Recall" = median(sapply(resultsBayes, "[[", 2)),
-        "F-Measure" = median(sapply(resultsBayes, "[[", 3))
-      )
-    )
-  
-  resultsUnbal <-
-    rbind(
-      resultsUnbal,
-      data.frame(
-        "Project" = project,
-        "Algorithm" = "SVM",
-        Precision = median(sapply(resultsSVM, "[[", 1)),
-        "Recall" = median(sapply(resultsSVM, "[[", 2)),
-        "F-Measure" = median(sapply(resultsSVM, "[[", 3))
-      )
-    )
-  
-  resultsUnbal <-
-    rbind(
-      resultsUnbal,
-      data.frame(
-        "Project" = project,
-        "Algorithm" = "Ramdom Forest",
-        Precision = median(sapply(resultsRF, "[[", 1)),
-        "Recall" = median(sapply(resultsRF, "[[", 2)),
-        "F-Measure" = median(sapply(resultsRF, "[[", 3))
-      )
-    )
-  
-}
-
-
-#Finals Results Bal:
-resultsBal
 
 #Finals Results Unbal:
-resultsUnbal
+write.csv(x = results, file = "ml_unbalanced.csv")
 
-
-#Utilizar técnicas de seleção de atributos para saber qual atributo é o mais importante;
-#
